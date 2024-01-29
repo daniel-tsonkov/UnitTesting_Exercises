b9393b078abdeb8b577d42799956d0fd977ba3c8
dan <daniel_tzonkov@abv.bg>
2024-01-29 16:06:22 +0200

    UP

--- src/test/java/p01_Database/DatabaseTest.java
+++ src/test/java/p01_Database/DatabaseTest.java
@@ -8,12 +8,17 @@ import javax.naming.OperationNotSupportedException;
 public class DatabaseTest {
     @Test
     public void testConstructorHasCreateCorrectObject() throws OperationNotSupportedException {
-        Integer [] numbers = {3, 4, 5, 8, 9, 10};
+        Integer[] numbers = {3, 4, 5, 8, 9, 10};
         Database database = new Database(numbers);
-        Integer [] databaseElemets = database.getElements();
+        Integer[] databaseElemets = database.getElements();
 
         Assert.assertArrayEquals("Arrays are not the same", numbers, databaseElemets);
         Assert.assertEquals("Count of element is incorect", database.getElementsCount(), numbers.length);
-        Assert.assertEquals("Index is incorect", numbers.length - 1, database.getIndex() );
+        Assert.assertEquals("Index is incorect", numbers.length - 1, database.getIndex());
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public class testConstructorThrowExceptionMoreThanSixteenElements() {
+
     }
 }
