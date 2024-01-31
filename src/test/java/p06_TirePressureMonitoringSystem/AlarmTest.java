644463d7edd810ebaf3347f3c97a643f015e866a
dan <daniel_tzonkov@abv.bg>
2024-01-31 10:05:58 +0200

    UP

--- src/test/java/p06_TirePressureMonitoringSystem/AlarmTest.java
+++ src/test/java/p06_TirePressureMonitoringSystem/AlarmTest.java
@@ -1,4 +1,17 @@
 package p06_TirePressureMonitoringSystem;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AlarmTest {
+    //presure is between 17 and 21
+    @Test
+    public void testPresureBetwen17And21() {
+        Sensor sensor = new Sensor();
+        Alarm alarm = new Alarm(sensor);
+
+        alarm.check();
+
+        Assert.assertFalse(alarm.getAlarmOn());
+    }
 }
