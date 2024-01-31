626e148549701e8a9022b5e8aef8b27ceea6f0a3
dan <daniel_tzonkov@abv.bg>
2024-01-31 09:51:09 +0200

    UP

--- src/test/java/p04_BubbleSortTest/BubbleTest.java
+++ src/test/java/p04_BubbleSortTest/BubbleTest.java
@@ -1,11 +1,15 @@
 package p04_BubbleSortTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BubbleTest {
     @Test
     public void testBubbleSort() {
-        int [] numbers = {5, 2, 4, 1, 3, 6, 8, 7};
+        int[] numbers = {5, 2, 4, 1, 3, 6, 8, 7};
+        Bubble.sort(numbers);
+        int [] expected = {1, 2, 3, 4, 5, 6, 7, 8};
 
+        Assert.assertArrayEquals(numbers, expected);
     }
 }
