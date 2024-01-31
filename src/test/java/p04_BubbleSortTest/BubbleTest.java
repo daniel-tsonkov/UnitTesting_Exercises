eee7f19d32f7a194848ce388daa755146736915f
dan <daniel_tzonkov@abv.bg>
2024-01-31 09:46:47 +0200

    UP

--- src/test/java/p04_BubbleSortTest/BubbleTest.java
+++ src/test/java/p04_BubbleSortTest/BubbleTest.java
@@ -1,4 +1,11 @@
 package p04_BubbleSortTest;
 
+import org.junit.Test;
+
 public class BubbleTest {
+    @Test
+    public void testBubbleSort() {
+        int [] numbers = {5, 2, 4, 1, 3, 6, 8, 7};
+
+    }
 }
