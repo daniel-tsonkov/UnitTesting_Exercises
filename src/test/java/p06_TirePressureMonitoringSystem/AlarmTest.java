e7ffbcfce6570a09263bb59ad417df6acdfda94c
dan <daniel_tzonkov@abv.bg>
2024-01-31 10:30:20 +0200

    UP

--- src/test/java/p06_TirePressureMonitoringSystem/AlarmTest.java
+++ src/test/java/p06_TirePressureMonitoringSystem/AlarmTest.java
@@ -21,4 +21,21 @@ public class AlarmTest {
         //System.out.println(sensor.readPressureSample());
         Assert.assertFalse(alarm.getAlarmOn());
     }
+
+    //presure is < 17
+    @Test
+    public void testPresureIsLessThan17() {
+        //for random generated value
+        //Sensor sensor = new Sensor();
+
+        //when we pick the value
+        Sensor sensor = Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(16.1);
+        Alarm alarm = new Alarm(sensor);
+
+        alarm.check();
+
+        //System.out.println(sensor.readPressureSample());
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
 }
